fbe55acab6d7a457daf8921a379ccdf4a1161c52
ForgottenScream <150345575+ForgottenScream@users.noreply.github.com>
2025-04-02 11:40:44 +0100

    finished first frame and made it into a child class

--- Main.java
+++ Main.java
@@ -1,26 +1,8 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class Main {
 	public static void main(String[] args) {
-		// JFrame = GUI window to add components to
-		JFrame frame = new JFrame(); //creates a frame
-
-		//JFrame Title
-		frame.setTitle("JFrame title goes here"); // sets title of frame
-
-		//Window Close Operations
-		//JFrame.HIDE_ON_CLOSE - default - runs in background when closed
-		//JFrame.DO_NOTHING_ON_CLOSE - does as it says on tin, don't use that
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //exit out of application
-
-		//Size of Window
-		frame.setSize(800, 600); // sets the x-dimension and y-dimension of frame
-		frame.setResizable(false); //prevent frame from being resized - cant be put to fullscreen either so be careful
-
-		//Image Icon for Window
-		ImageIcon icon = new ImageIcon("tux.png");
-
-
-		frame.setVisible(true); // makes frame visible
+		MyFrame myFrame = new MyFrame();
 	}
 }
---
+++ MyFrame.java
@@ -0,0 +1,31 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class MyFrame extends JFrame {
+	MyFrame() {
+		// JFrame = GUI window to add components to
+		//JFrame Title
+		this.setTitle("JFrame title goes here"); // sets title of frame
+
+		//Window Close Operations
+		//JFrame.HIDE_ON_CLOSE - default - runs in background when closed
+		//JFrame.DO_NOTHING_ON_CLOSE - does as it says on tin, don't use that
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //exit out of application
+
+		//Size of Window
+		this.setSize(800, 600); // sets the x-dimension and y-dimension of frame
+		this.setResizable(false); //prevent frame from being resized - cant be put to fullscreen either so be careful
+
+		//Image Icon for Window
+		ImageIcon icon = new ImageIcon("tux.png"); //creates an ImageIcon
+		this.setIconImage(icon.getImage()); //change icon of frame
+
+		//Customize appearance
+		this.getContentPane().setBackground(new Color(19, 115, 184)); //change background color (color name, rgb, hex value)
+
+
+
+
+		this.setVisible(true); // makes frame visible
+	}
+}
