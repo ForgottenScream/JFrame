1728e3865a6f92c22b9cd521f500b71ac34b1743
ForgottenScream <150345575+ForgottenScream@users.noreply.github.com>
2025-04-02 20:46:16 +0100

    finished flow layout

--- Main.java
+++ Main.java
@@ -2,8 +2,8 @@ public class Main {
 	public static void main(String[] args) {
 //		MyFrame myFrame = new MyFrame(); //very basics
 //		MyPanel panel = new MyPanel(); //looking at JPanels now
-//		MyButton button = new MyButton();
-//		MyBorders myBorders = new MyBorders();
-		MyFlow myFlow = new MyFlow();
+//		MyButton button = new MyButton(); //basic buttons
+//		MyBorders myBorders = new MyBorders(); // using BorderLayout
+//		MyFlow myFlow = new MyFlow(); // using FlowLayout
 	}
 }
--- MyFlow.java
+++ MyFlow.java
@@ -1,8 +1,34 @@
 import javax.swing.*;
+import java.awt.*;
 
-public class MyFlow extends JPanel {
+public class MyFlow extends JFrame {
 
 	// FlowLayout = places components in a row, sized at their preferred size.
 	//              If the horizontal space in the container is too small,
 	//              the FlowLayout class uses the next available row.
+
+	public MyFlow() {
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setSize(500, 500);
+		this.setLayout(new FlowLayout(FlowLayout.LEADING, 10, 10));
+
+		JPanel panel = new JPanel();
+		panel.setPreferredSize(new Dimension(100, 250));
+		panel.setBackground(Color.lightGray);
+		panel.setLayout(new FlowLayout());
+
+		panel.add(new JButton("1"));
+		panel.add(new JButton("2"));
+		panel.add(new JButton("3"));
+		panel.add(new JButton("4"));
+		panel.add(new JButton("5"));
+		panel.add(new JButton("6"));
+		panel.add(new JButton("7"));
+		panel.add(new JButton("8"));
+		panel.add(new JButton("9"));
+
+
+		this.add(panel);
+		this.setVisible(true);
+	}
 }
