51a9e53a513324c2fc160aedd4a8da0c74506fa3
ForgottenScream <150345575+ForgottenScream@users.noreply.github.com>
2025-04-03 01:19:31 +0100

    saving

--- MyLayeredPane.java
+++ MyLayeredPane.java
@@ -25,9 +25,10 @@ public class MyLayeredPane extends TemplateFrame {
 
 		JLayeredPane layeredPane = new JLayeredPane();
 		layeredPane.setBounds(0, 0, 500, 500);
-		layeredPane.add(label1);
-		layeredPane.add(label2);
-		layeredPane.add(label3);
+
+		layeredPane.add(label1, JLayeredPane.DEFAULT_LAYER);
+		layeredPane.add(label2, JLayeredPane.DEFAULT_LAYER);
+		layeredPane.add(label3, JLayeredPane.DRAG_LAYER);
 
 		this.add(layeredPane);
 
